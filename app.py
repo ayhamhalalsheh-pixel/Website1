4780c4fa8f31b8a3aa474a194b861834cec423a6
ayhamhalalsheh-pixel <ayham.halalsheh@gmail.com>
2025-09-09 11:02:02 +0000

    added the flask project

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, ayham!' 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True) 
